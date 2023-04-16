94a472df5a0223e67946159483c611018469b017
Vishal T <vishalvtakale@gmail.com>
2023-04-16 17:50:28 +0530

    Adding Multiplication code to the dev branch

---
+++ Multiply.java
@@ -0,0 +1,8 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int a=5;
+        int b=5;
+
+        System.out.println("Multiplication of two no is = "+a*b);
+    }
+}
